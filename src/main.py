9e387b32df895fa55542eb9b707d1f8e5a7bca1c
Rubeena-a <rubeenashaik889@gmail.com>
2025-10-24 16:17:09 +0000

    updating main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Main application file for DevOps Project
+"""
+
+def main():
+    print("Welcome to DevOps Project!")
+    print("Version: 1.0.0")
+    
+if __name__ == "__main__":
+    main()
